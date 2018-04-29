18ad7bc61b715c40546799fd4189b32b7c8a6ca5
shivam123425 <shivam123425@gmail.com>
2018-04-29 22:48:06 +0530

    Add env variables to app.listen

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/", (req,res)=> {
 })
 
 
-app.listen(8080, ()=> {
-    console.log("Server Running");
-})
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("Server has started");
+});
